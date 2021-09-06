f43e6a71888cd79bc554b1618f638f00ad6e84a5
GermanSan2002 <germansan2002@outlook.com>
2021-09-06 11:44:21 -0300

    Principal.java-----Declaracion de variables

---
+++ TP1--Programacion2--Grupo17/src/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal {
+    public static void main(String[] args) {
+        int entero;
+        float flotante;
+        double doble;
+        String cadena;
+        
+    }
+}
